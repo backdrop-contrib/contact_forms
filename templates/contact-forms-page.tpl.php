0897ad0d710612083ebd00f8ddf98059814ae333
behindthepage <geoff@behindthepage.com.au>
2015-05-13 18:59:47 +1000

    Fix bug with category edit form and remove unneeded hook_form_validate

--- templates/contact-forms-page.tpl.php
+++ templates/contact-forms-page.tpl.php
@@ -5,15 +5,29 @@
  *
  * Available variables:
  * - $categories: Array of all contact categories.
+ *
+ * Available variables: for each contact categories
+ * - $cat['cid'],
+ * - $cat['category'],
+ * - $cat['page_title'],
+ * - $cat['page_info'],
+ * - $cat['path'],
+ * - $cat['link'],
  */
+?>
+<?php
 foreach ($categories as $key => $cat){
-    $cat = contact_forms_prepare_category($cat);
-    print theme('contact_forms_contact', array(
-      'cid' => $cat['cid'],
-      'category' => $cat['category'],
-      'page_title' => $cat['page_title'],
-      'page_info' => $cat['page_info'],
-      'path' => $cat['path'],
-      'link' => $cat['link'],
-      ));
-  }
+  $cat = contact_forms_prepare_category($cat);
+  // To see the variables available uncomment this block
+  /*
+  print '<pre>';
+  print_r($cat);
+  print '</pre>';
+  */
+?>
+<div class="cf-category cat-<?php print $cat['cid']; ?>">
+  <h2><?php print $cat['category']; ?></h2>
+  <div class="cf-title"><?php print $cat['page_info']; ?></div>
+  <div class="cf-link"><?php print $cat['link']; ?></div>
+</div>
+<?php } ?>
